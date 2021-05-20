142f989d42262ec267e98ab5ed7281b38a2537af
bybrisk <imctobybrisk@gmail.com>
2021-05-20 18:51:40 +0530

    callback changed

--- handlers/login.go
+++ handlers/login.go
@@ -32,7 +32,7 @@ var (
 
 func init() {
 	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:8080/auth/google/callback",
+		RedirectURL:  "http://localhost:8080/meeting/get/all/callback",
 		ClientID:     "113188653176-fjoovrjckjns6hk9p9nunnp3677omhb3.apps.googleusercontent.com",
 		ClientSecret: "C2b3yeljmmSW-rn5WEGJ17kl",
 		Scopes:       []string{"https://www.googleapis.com/auth/calendar.events"},
