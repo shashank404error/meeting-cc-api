37765d787c733b5c44f4eae7b9d3ac18e0ba2962
bybrisk <imctobybrisk@gmail.com>
2021-05-20 18:57:20 +0530

    init added

--- data/eventDetailCallback.go
+++ data/eventDetailCallback.go
@@ -22,6 +22,16 @@ var (
 	oauthStateString = "pseudo-random"
 )
 
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/meeting/get/all/callback",
+		ClientID:     "113188653176-fjoovrjckjns6hk9p9nunnp3677omhb3.apps.googleusercontent.com",
+		ClientSecret: "C2b3yeljmmSW-rn5WEGJ17kl",
+		Scopes:       []string{"https://www.googleapis.com/auth/calendar.events"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
 func EventDetailCallback (r *http.Request) {
 	_, token, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
 	if err != nil {
